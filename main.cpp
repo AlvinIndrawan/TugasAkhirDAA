b78bf2fb14775cfbd95f46660597cd0eb90447ef
noviyana12 <54648155+noviyana12@users.noreply.github.com>
2021-06-22 19:48:19 +0700

    Penambahan struct untuk sistem linked list

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+//struct untuk penggunaan linked list
+struct produk{
+	int harga_produk, stok_produk;
+	string id_produk, nama_produk;
+	
+	produk *next;
+};
+
+produk *newData, *head=NULL, *tail=NULL, *tampil, *temp;
+
+
 int main() {
 	
 	cout << "Ini program penjualan DAA\n";
